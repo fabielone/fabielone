467d83980f407cf0363bb8adf11a228c2f42e5d0
fabiel <fabielone@gmail.com>
2023-10-02 21:27:51 -0700

    fix homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,59 +1,80 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import Footer from "~/components/FooterComp";
 import ContComp from "~/components/ContComp";
-import DescComp from "~/components/DescComp";
 import HeaderComp from "~/components/HeaderComp";
 import PicComp from "~/components/PicComp";
 import type { V2_MetaFunction } from "@remix-run/node";
+import DescComp from "~/components/DescComp";
 
 export const meta: V2_MetaFunction = () => [{ title: "Fabiel" }];
 
-export default function Index() {
+export default function In() {
   return (
     <Grid
       templateAreas={[
-        `"pic"
-                        "nav"
-                       "main"
-                       "desc"
-                       "footer"`,
-        `"pic"
-                        "nav"
-                       "main"
-                       "desc"
-                       "footer"`,
-        `"pic"
-                        "nav"
-                       "main"
-                       "desc"
-                       "footer"`,
         `"pic nav"
-                        "desc main"
-                        "footer footer"`,
+   "main main"
+   "desc desc"
+   "footer footer"`,
+        `"pic nav"
+   "main"
+   "desc"
+   "footer"`,
+        `"pic nav"
+   "main"
+   "desc"
+   "footer"`,
+        `"pic nav"
+    "desc main"
+    "footer footer"`,
       ]}
-      gridTemplateRows={"35vh 40vh 15vh"}
-      gridTemplateColumns={["100vh", "30vw 1fr"]}
-      h="100%"
-      w="100%"
-      gap="1"
+      gridTemplateRows={[
+        "35vh fit-content 40vh 15vh",
+        "35vh fit-content 40vh 15vh",
+        "35vh fit-content 40vh 15vh",
+        "35vh 40vh 18vh",
+      ]}
+      gridTemplateColumns={[
+        "40vw 55vw",
+        "40vw 55vw ",
+        "30vw 65vw",
+        "30vw 65vw",
+      ]}
+     // h={["132vh","132vh","132vh","94vh"]} // Use viewport height to ensure the layout fits the screen
+     h={'fit-content'} 
+     w="96%"
       color="blackAlpha.700"
       fontWeight="bold"
-      mx={[3]}
-      my={[3]}
+      mx={["2%"]} // Adjust margin as needed
+      //my={['2%']} // Adjust margin as needed
+      //marginTop={['10px']} // Adjust margin as needed
+      border={"3px solid #BDBDBD"}
+      borderRadius={"lg"}
     >
-      <GridItem pl="2" bg="orange.300" area={"pic"}>
+      <GridItem
+        pl={["4", "52px", "100px", "100px"]}
+        bg=""
+        area={"pic"}
+        alignSelf={"center"}
+      >
         <PicComp></PicComp>
       </GridItem>
-      <GridItem pl="2" bg="pink.300" area={"nav"}>
+      <GridItem pl="2" bg="" area={"nav"}>
         <HeaderComp></HeaderComp>
       </GridItem>
-      <GridItem pl="2" bg="green.300" area={"desc"}>
+      <GridItem pl="2" bg="" area={"desc"}>
         <ContComp></ContComp>
       </GridItem>
-      <GridItem pl="2" bg="blue.300" area={"main"}>
-        <DescComp />
+      <GridItem pl="2" bg="" area={"main"}>
+      <DescComp />
       </GridItem>
-      <GridItem pl="2" bg="blue.300" area={"footer"}>
+      <GridItem
+        pl="2"
+        bg=""
+        area={"footer"}
+        display={"flex"}
+        alignSelf={"flex-end"}
+      >
         <Footer />
       </GridItem>
     </Grid>
