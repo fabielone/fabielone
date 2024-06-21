60f27f7b2fd5274a8b859ccf734f23db6294b431
Fabiel Ramirez Garcia <fabiel@Fabiels-MacBook.local>
2024-06-20 21:15:03 -0700

    updated drawer

--- app/components/molecules/Homepage/NavBar.tsx
+++ app/components/molecules/Homepage/NavBar.tsx
@@ -34,6 +34,7 @@ export const NavBar = ({ user }: { user: User | undefined }) => {
   const toggleDrawer = () => setIsDrawerOpen(!isDrawerOpen);
   const closeDrawer = () => setIsDrawerOpen(false);
 
+
   return (
     <>
       <nav className={`w-full z-10 ${isHomePage ? 'absolute top-0 left-0 bg-transparent' : 'relative bg-white shadow-md'}`}>
@@ -56,7 +57,7 @@ export const NavBar = ({ user }: { user: User | undefined }) => {
             ) : (
               <>
                 <Link to="/login" className="text-sm font-bold text-gray-500 drop-shadow-md">Login</Link>
-                <span className="text-sm text-white font-bold"> | </span>
+                <span className="text-sm text-gray font-bold"> | </span>
                 <Link to="/join" className="text-sm font-bold text-gray-500">Sign Up</Link>
               </>
             )}
@@ -101,37 +102,112 @@ export const NavBar = ({ user }: { user: User | undefined }) => {
           </div>
         </div>
       </nav>
-      {isDrawerOpen ? <div className="fixed inset-0 bg-gray-800 bg-opacity-50 z-20" onClick={closeDrawer}>
-          <div className="fixed inset-y-0 left-0 w-64 bg-white shadow-md transform transition-transform duration-300 ease-in-out translate-x-0">
-            <div className="p-4">
-              <button
-                type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                onClick={closeDrawer}
-              >
-                <span className="sr-only">Close menu</span>
-                ✕
-              </button>
-            </div>
-            <ul className="flex flex-col font-medium p-4 space-y-4">
-              <li>
-                <Link to="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" onClick={closeDrawer}>Home</Link>
-              </li>
-              <li>
-                <Link to="/about" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" onClick={closeDrawer}>About</Link>
-              </li>
-              <li>
-                <Link to="/services" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" onClick={closeDrawer}>Services</Link>
-              </li>
-              <li>
-                <Link to="/pricing" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" onClick={closeDrawer}>Pricing</Link>
-              </li>
-              <li>
-                <Link to="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" onClick={closeDrawer}>Contact</Link>
-              </li>
-            </ul>
+     {/* Drawer for mobile */}
+     <div className={`fixed inset-0 z-40 ${isDrawerOpen ? 'block' : 'hidden'}`} role="dialog">
+        <button className="fixed inset-0 bg-gray-800 bg-opacity-50" onClick={closeDrawer}></button>
+        <div className="fixed inset-y-0 left-0 w-64 bg-white dark:bg-gray-800 overflow-y-auto transform transition-transform duration-300 ease-in-out">
+          <div className="p-4">
+            <button
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              onClick={closeDrawer}
+            >
+              <span className="sr-only">Close menu</span>
+              ✕
+            </button>
           </div>
-        </div> : null}
+          <ul className="flex flex-col font-medium p-4 space-y-4">
+            {subMenuItems.map((item) => (
+              <Link key={item.path} to={item.path} className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700`} onClick={closeDrawer}>
+                {item.name}
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+     
+      
+
+            {/* Drawer component */}
+            <div
+                id="drawer-navigation"
+                className={`fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform ${
+                    isDrawerOpen ? '' : '-translate-x-full'
+                } bg-white w-64 dark:bg-gray-800`}
+                tabIndex={-1}
+                aria-labelledby="drawer-navigation-label"
+            >
+                <h5 id="drawer-navigation-label" className="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">
+                    Menu
+                </h5>
+                <button
+                    type="button"
+                    data-drawer-hide="drawer-navigation"
+                    aria-controls="drawer-navigation"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    onClick={closeDrawer}
+                >
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span className="sr-only">Close menu</span>
+                </button>
+                <div className="py-4 overflow-y-auto">
+                    <ul className="space-y-2 font-medium">
+                        <li>
+                            <a href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                                </svg>
+                                <span className="ms-3">Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <button
+                                type="button"
+                                className="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                aria-controls="dropdown-example"
+                                data-collapse-toggle="dropdown-example"
+                            >
+                                <svg
+                                    className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor"
+                                    viewBox="0 0 18 21"
+                                >
+                                    <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z"/>
+                                </svg>
+                                <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">E-commerce</span>
+                                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+                                </svg>
+                            </button>
+                            <ul id="dropdown-example" className="hidden py-2 space-y-2">
+                                <li>
+                                    <a href="/" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
+                                </li>
+                                <li>
+                                    <a href="/" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Billing</a>
+                                </li>
+                                <li>
+                                    <a href="/" className="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="/" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
+                                </svg>
+                                <span className="ms-3">Analytics</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
     </>
   );
 };
