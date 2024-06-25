a8e785c1bdc865c7b92201c14af5af54f68f0853
Fabiel Ramirez Garcia <fabiel@Fabiels-MacBook.local>
2024-06-25 16:26:06 -0700

    fixed link error , commented remarkplugings on vite config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,14 +4,14 @@
 import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
 import { rehypePrettyCode } from "rehype-pretty-code";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkMdxFrontmatter from "remark-mdx-frontmatter";
+// import remarkFrontmatter from "remark-frontmatter";
+// import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [ tsconfigPaths(),{enforce:'pre', ...mdx({
-    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    //remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
     rehypePlugins: [rehypePrettyCode],
   })},react(),],
   test: {
