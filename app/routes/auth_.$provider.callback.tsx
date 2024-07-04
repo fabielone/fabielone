0f330c801a2f6b013d016dcf2b1fb2bcf9152af0
Fabiel Ramirez Garcia <fabiel@Fabiels-MacBook.local>
2024-07-04 11:07:37 -0700

    changes log in

--- app/routes/auth_.$provider.callback.tsx
+++ app/routes/auth_.$provider.callback.tsx
@@ -1,14 +1,20 @@
 // app/routes/auth/$provider.tsx
-import { ActionFunctionArgs, redirect } from "@remix-run/node"
+import { ActionFunction,LoaderFunction} from "@remix-run/node"
+
+import { createUserSession } from "~/session.server";
 
 import { authenticator } from '../services/auth.server';
 
-export const loader = () => redirect('/login');
-
-export const action = ({ request, params }: ActionFunctionArgs) => {
-    const provider = params.provider || ""; // Handle the case where params.provider is undefined
-  return authenticator.authenticate(provider, request, {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login',
+export const loader: LoaderFunction = async ({ request }) => {
+  return authenticator.authenticate("google", request, {
+    successRedirect: "/",
+    failureRedirect: "/login",
   });
+};
+
+export const action: ActionFunction = async ({ request }) => {
+  const user = await authenticator.authenticate("google", request);
+  const session = await sessionStorage.getSession(request.headers.get("Cookie"));
+  const redirectTo = session.get("redirectTo") || "/";
+  return createUserSession({ request, userId: user.id, remember: true, redirectTo });
 };
