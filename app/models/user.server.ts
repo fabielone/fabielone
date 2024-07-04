05417f87ca5ddc81c901ad01ce34e047f3b2019f
Fabiel Ramirez Garcia <fabiel@Fabiels-MacBook.local>
2024-07-04 10:54:44 -0700

    updated createusersocial and adding password

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -65,8 +65,14 @@ export async function createUser(
 export async function createUserSocial(
   email: User["email"],
 ) {
-  const db = await arc.tables();
+  const hashedPassword = await bcrypt.hash("password", 10);
 
+ 
+  const db = await arc.tables();
+  await db.password.put({
+    pk: `email#${email}`,
+    password: hashedPassword,
+  });
   await db.user.put({
     pk: `email#${email}`,
     email,
