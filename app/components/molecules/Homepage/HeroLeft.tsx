de1a5520d1aac152d9c8a1b1648474ba68a236bd
Fabiel Ramirez Garcia <fabiel@Fabiels-MacBook.local>
2024-07-17 13:30:08 -0700

    almost done with hero

--- app/components/molecules/Homepage/HeroLeft.tsx
+++ app/components/molecules/Homepage/HeroLeft.tsx
@@ -11,42 +11,45 @@ interface HeroLeftProps {
 
 export default function HeroLeft({ heading, punchline, pills }: HeroLeftProps) {
   return (
-    <div className="relative h-full px-4 pt-16 mx-auto lg:py-16 md:px-8 xl:px-20 bg-gray-50 border">
-      <div className="max-w-xl h-full mx-auto  lg:flex">
-        <div className="mb-16 h-full  lg:mb-0 lg:ml-8">
-          <div className="max-w-xl mb-6">
-            <div className="flex items-start mb-4 h-full border-b border-gray-300 pb-4">
-              <div className="p-1 rounded-lg border-2 border-gray-500">
-                <Avatar img="https://fabielone.s3.us-west-1.amazonaws.com/portfolio/profile.jpeg" className="rounded-lg" />
+    <div className="relative h-full px-4 pt-16 mx-auto lg:py-16 bg-gray-50 border">
+      <div className="max-w-xl h-full mx-auto lg:flex">
+        <div className="mb-16 h-full lg:mb-0">
+          <div className="bg-white border rounded-lg p-6 h-full flex flex-col">
+            <div className="max-w-xl mb-6">
+              <div className="flex items-start mb-4 h-full border-b border-gray-300 pb-4">
+                <div className="p-1 rounded-lg border-2 border-gray-500">
+                  <Avatar img="https://fabielone.s3.us-west-1.amazonaws.com/portfolio/profile.jpeg" className="rounded-lg" />
+                </div>
+                <div className="flex flex-col ml-4">
+                  <p className="text-lg font-semibold text-gray-900">{heading}</p>
+                  <p className="text-md text-gray-700">Software Engineer</p>
+                </div>
               </div>
-              <div className="flex flex-col ml-4">
-                <p className="text-lg font-semibold text-gray-900">{heading}</p>
-                <p className="text-md text-gray-700">Software Engineer</p>
+              <h1 className="text-2xl font-bold text-gray-900 text-center mb-4">
+                Fabiel: Expert Web Development and Online Business Solutions in Tijuana
+              </h1>
+              <div className="flex flex-wrap space-x-2 mb-4 justify-center">
+                {pills.map((pill) => (
+                  <Pills key={pill.text} text={pill.text} color={pill.color} bgColor={pill.bgColor} />
+                ))}
               </div>
+              <hr className="border-t border-gray-300 my-4" />
+              <p className="text-xl text-gray-600 mt-2 mb-4">{punchline}</p>
             </div>
-        
-            <h1 className="text-2xl font-bold text-gray-900 text-center mb-4">
-              Fabiel: Expert Web Development and Online Business Solutions in Tijuana
-            </h1>
-            <div className="flex flex-wrap space-x-2 mb-4 justify-center">
-              {pills.map((pill) => (
-                <Pills key={pill.text} text={pill.text} color={pill.color} bgColor={pill.bgColor} />
-              ))}
-            </div>
+            <div className="flex-grow"></div>
             <hr className="border-t border-gray-300 my-4" />
-            <p className="text-xl text-gray-600 mt-2 mb-4">{punchline}</p>
-          </div>
-          <div className="flex items-center mb-6 space-x-4 justify-center">
-            <Button className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500">
-              Free Consultation
-              <IoIosCalendar className="ml-2 h-5 w-5" />
-            </Button>
-            <a
-              href="/contact"
-              className="inline-flex items-center font-semibold transition-colors duration-200 text-indigo-600 hover:text-indigo-800"
-            >
-              Contact Form
-            </a>
+            <div className="flex items-center mb-6 space-x-4 justify-center">
+              <Button className="bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500">
+                Free Consultation
+                <IoIosCalendar className="ml-2 h-5 w-5" />
+              </Button>
+              <a
+                href="/contact"
+                className="inline-flex items-center font-semibold transition-colors duration-200 text-indigo-600 hover:text-indigo-800"
+              >
+                Contact Form
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- app/components/molecules/Homepage/HeroRight.tsx
+++ app/components/molecules/Homepage/HeroRight.tsx
@@ -1,4 +1,4 @@
-import { FaMountain, FaChalkboardTeacher, FaPaintBrush } from "react-icons/fa";
+import { FaMountain, FaChalkboardTeacher, FaPaintBrush, FaGamepad, FaExternalLinkAlt } from "react-icons/fa";
 
 export default function HeroRight() {
   return (
@@ -8,16 +8,34 @@ export default function HeroRight() {
           <div className="bg-white border rounded-lg overflow-hidden flex flex-col h-full w-full max-w-xl">
             <img className="w-full h-64 object-cover object-center" src="https://fabielone.s3.us-west-1.amazonaws.com/portfolio/heroleft" alt="Profile" />
             <div className="p-6 flex-grow">
-              <h2 className="text-2xl font-semibold text-gray-800">Your Name</h2>
+              <h2 className="text-2xl font-semibold text-gray-800">Get to Know Me</h2>
               <p className="text-gray-600 mt-4">
-                🌟 Based in <span className="text-yellow-300">Tijuana, Mexico</span>, a vibrant city in Baja California, I have
-                years of experience in development and a passion for crafting detailed solutions. 
+                🌟 Hi there! I&apos;m based in the lively city of <span className="text-blue-700">Tijuana, Mexico</span>, nestled in Baja California. With years of experience in development, I love creating detailed and effective solutions. 
+                <br /> I am continuously working with clients, hosting webinars, and teaching the new generation of coders.
                 <span className="block mt-4">
-                  <FaMountain className="inline-block mr-2 text-2xl text-yellow-300" /> I enjoy the <span className="text-yellow-300">outdoors</span>,
-                  <FaChalkboardTeacher className="inline-block mx-2 text-2xl text-yellow-300" /> <span className="text-yellow-300">teaching</span>, and
-                  <FaPaintBrush className="inline-block mx-2 text-2xl text-yellow-300" /> <span className="text-yellow-300">painting</span>.
+                  <FaMountain className="inline-block mr-2 text-2xl text-blue-700" /> I enjoy the <span className="text-blue-700">outdoors</span>
+                  <a href="https://www.fabiel.one/lifestyle-blog" className="inline-block ml-2">
+                    <FaExternalLinkAlt className="inline-block text-2xl text-blue-700" />
+                  </a>,
+                  <FaChalkboardTeacher className="inline-block mx-2 text-2xl text-blue-700" /> <span className="text-blue-700">teaching</span>
+                  <a href="https://www.fabiel.one/classes" className="inline-block ml-2">
+                    <FaExternalLinkAlt className="inline-block text-2xl text-blue-700" />
+                  </a>, 
+                  <FaPaintBrush className="inline-block mx-2 text-2xl text-blue-700" /> <span className="text-blue-700">painting</span>
+                  <a href="https://www.fabiel.one/store" className="inline-block ml-2">
+                    <FaExternalLinkAlt className="inline-block text-2xl text-blue-700" />
+                  </a>, and
+                  <FaGamepad className="inline-block mx-2 text-2xl text-blue-700" /> <span className="text-blue-700">gaming</span>
+                  <a href="https://www.fabiel.one/gaming" className="inline-block ml-2">
+                    <FaExternalLinkAlt className="inline-block text-2xl text-blue-700" />
+                  </a>.
                 </span>
               </p>
+              <div className="mt-6">
+                <a href="https://www.fabiel.one/profile" className="inline-block px-6 py-2 bg-blue-700 text-white font-semibold rounded hover:bg-blue-800 transition">
+                  View Profile
+                </a>
+              </div>
             </div>
           </div>
         </div>
