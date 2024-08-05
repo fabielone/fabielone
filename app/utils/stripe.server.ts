68cd672d09c68e3f435b7dd32f52a69989b5bb36
Fabiel Ramirez Garcia <fabiel@Fabiels-MacBook.local>
2024-08-05 09:41:52 -0700

    some more pruning

--- app/utils/stripe.server.ts
+++ app/utils/stripe.server.ts
@@ -1,7 +1,7 @@
-// utils/stripe.server.ts
-import Stripe from 'stripe';
+// // utils/stripe.server.ts
+// import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
-  typescript:true,
-});
+// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: '2024-06-20',
+//   typescript:true,
+// });
